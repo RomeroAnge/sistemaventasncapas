22bfd8565e7d9b2292d019d6357cc6e3f2648a8c
Angeloxd <angelrf186@gmail.com>
2024-03-12 10:25:02 -0400

    Commi 5to Creacion de Clases

---
+++ SistemasVentas/SistemasVentas.Modelos/DetalleEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.Modelos
+{
+    public class DetalleEvent
+    {
+        public int IdDetalleEvent { get; set; }
+        public int IdVenta { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public float PrecioVenta { get; set; }
+        public float SubTotal { get; set; }
+        public string Estado { get; set; }
+    }
+}
