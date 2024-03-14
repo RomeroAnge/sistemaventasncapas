9144110c94d34bc7d12fe119a8a8ad223cbeed37
Angeloxd <angelrf186@gmail.com>
2024-03-13 21:36:44 -0400

    Commit 7mo v2 Conexión de la Base de Datos y Metodo Listar

---
+++ SistemasVentas/SistemasVentas.DAL/DetalleIngDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.DAL
+{
+    public class DetalleIngDal
+    {
+        public DataTable ListarDetalleIngDal()
+        {
+            string consulta = "select * from detalleing";
+            DataTable lista = conexion.EjecutarDataTabla(consulta, "tabla");
+            return lista;
+        }
+    }
+}
---
+++ SistemasVentas/SistemasVentas.DAL/IngresoDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.DAL
+{
+    public class IngresoDal
+    {
+        public DataTable ListarIngresoDal()
+        {
+            string consulta = "select * from ingreso";
+            DataTable lista = conexion.EjecutarDataTabla(consulta, "tabla");
+            return lista;
+        }
+    }
+}
